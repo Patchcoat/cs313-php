bacc658c73561292b9a2a79f207784c7e2ad1691
TheGreatS <spiencewelch@gmail.com>
2019-09-24 16:21:15 -0700

    Stretch Challenges

--- web/scripts.js
+++ web/scripts.js
@@ -1,13 +1,22 @@
-function clickedAlert() {
-    alert("Clicked!");
-}
-
-function changeColor() {
-    var color = document.forms["colorChange"]["color"].value;
-    if (color == "") {
-        alert("The text box needs something in it");
-        return false;
-    }
-    var div1 = document.getElementById('div1');
-    div1.style.backgroundColor = color;
-}
+$(document).ready(function() {
+    $("#clickAlert").click(function(){
+        alert("Clicked!");
+    });
+    $("#changeColor").click(function(){
+        var color = $("#colorInput").val();
+        if (color == "") {
+            alert("The text box needs something in it");
+            return false;
+        }
+        $("#div1").css("background-color", color);
+    });
+    var visible = true;
+    $("#toggleVis").click(function(){
+        visible = !visible;
+        if (visible) {
+            $("#div3").fadeIn("slow");
+        } else {
+            $("#div3").fadeOut("slow");
+        }
+    });
+});
