01d8b6302b01630a179d936a740abc934c3b6df1
TheGreatS <spiencewelch@gmail.com>
2019-09-28 13:23:51 -0700

    added javascript

--- web/home.js
+++ web/home.js
@@ -3,7 +3,7 @@ var ctx = canvas.getContext("2d");
 var radius = 50;
 ctx.fillStyle = "#000000";
 ctx.fillRect(0,0,500,500);
-ctx.fillStyle = "#FF0000";
 ctx.beginPath();
 ctx.arc(250, 50, radius, 0, 2* Math.PI, false);
+ctx.fillStyle = "#FF0000";
 context.fill();
