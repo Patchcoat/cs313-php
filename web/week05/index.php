23dc836e6325df87017b4db3d01e8ce11ad8c3fc
TheGreatS <spiencewelch@gmail.com>
2019-10-15 20:20:13 -0700

    update

--- web/week05/index.php
+++ web/week05/index.php
@@ -20,7 +20,7 @@ try {
     $dbPort = $dbOpts["port"];
     $dbUser = $dbOpts["user"];
     $dbPassword = $dbOpts["pass"];
-    $db = new PDO('pgsql:host=$dbHost;port=$dbPort;dbname=$dbName', $dbUser, $dbPassword);
+    $db = new PDO('pgsql:host='.$dbHost.';port='.$dbPort.';dbname='.$dbName, $dbUser, $dbPassword);
 
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
