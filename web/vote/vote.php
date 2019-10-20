b84f55bcd4aeb21d50d0b8c28dc4e1ca3c994c32
TheGreatS <spiencewelch@gmail.com>
2019-10-19 19:37:11 -0700

    updated database access for heroku

--- web/vote/vote.php
+++ web/vote/vote.php
@@ -19,11 +19,22 @@
  * */
 $poll = filter_var($_GET['poll'], FILTER_SANITIZE_NUMBER_INT);
 try {
+    $dbURL = getenv('DATABASE_URL');
+
+    $dbOpts = parse_url($dbURL);
+
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+
     $user = 'postgres';
     $password = 'password';
 
-    $db = new PDO('pgsql:host=localhost;port=5432;dbname=postgres', $user, $password);
+    $db = new PDO('pgsql:host=$dbHost;port=$dbPort;dbname=$dbName', $dbUser, $dbPassword);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
     foreach($db->query('SELECT url FROM polls WHERE ID='.$poll) as $row) {
         echo '<h1 class="display-1">'.$row['url'].' voting</h1>';
     }
